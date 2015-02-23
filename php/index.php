2bd83d93318db83d34def7faef972a2785107590
nikhiljindal <nikhiljindal18@gmail.com>
2015-02-23 13:14:25 -0800

    testing

--- php/index.php
+++ php/index.php
@@ -1,5 +1,6 @@
 <html>
 <body>
-<?php include('https://github.com/GoogleCloudPlatform/kubernetes/blob/master/api/swagger-spec/api.json'); ?>
+Hello World!
+<!-- ?php include('https://github.com/GoogleCloudPlatform/kubernetes/blob/master/api/swagger-spec/api.json'); ?-->
 </body>
 </html>
